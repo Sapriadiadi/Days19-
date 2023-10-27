ba4040e2ac6a6deda8b1dd092acd2c3758ed4a5b
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-28 06:22:32 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package days_19;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String firstName = "aku ";
+    String lastName = "kamu";
+    String fullName = firstName + lastName;
+    System.out.println(fullName); 
+  }
+
+    }
+   
+
+  
+
+    
+
